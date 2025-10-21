c1bb3e184ac66b870eade3d736472f1adb1c8cdd
developeranil65 <anilsingh.portfolio@gmail.com>
2025-10-21 09:35:59 +0530

    feat(api): backend scaffold

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+// Loading env configurations
+dotenv.config({
+    path: './.env'
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`Server is running at port : ${process.env.PORT}`);
+})
+app.on("error", (error) => {
+    console.log("Server Error", error);
+    throw error;
+})
